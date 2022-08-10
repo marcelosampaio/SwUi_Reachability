cd34b47c8a6bc322635cdf3078fcc94bd86166d1
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-08-10 08:52:10 -0300

    Initial Commit

---
+++ SwUi_Reachability/SwUi_ReachabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_ReachabilityApp.swift
+//  SwUi_Reachability
+//
+//  Created by Marcelo Sampaio on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_ReachabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
